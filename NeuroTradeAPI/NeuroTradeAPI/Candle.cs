b14ee6cea5ab8fd6edd6fb523baf81e05d00da94
awroma <romech95@gmail.com>
2017-11-21 00:40:07 +0300

    Replaced a model with "Candle"

---
+++ NeuroTradeAPI/NeuroTradeAPI/Candle.cs
@@ -0,0 +1,13 @@
+﻿namespace NeuroTradeAPI
+{
+    public class Candle
+    {
+        public int Id { get; set; }
+        public string Alias { get; set; }
+        public float Open { get; set; }
+        public float Close { get; set; }
+        public float Low { get; set; }
+        public float High { get; set; }
+        public int Volume { get; set; }
+    }
+}
