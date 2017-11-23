e7ea2e74b63aca47ffb7f437d63bea1c6a39e6a2
SergeyGorbatyuk171 <sergeygorbatyuk171@gmail.com>
2017-11-23 22:10:34 +0300

    first version of core downloader. Need to fix weird behavior with multiple tasks running

---
+++ CoreDownloader/CoreDownloader/Downloader.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentScheduler;
+
+namespace CoreDownloader
+{
+    public class Downloader
+    {
+        //private IQuoteStorage _storage { get; set; }
+        private MD5 md5Hash = MD5.Create();
+        HashSet<string> hashUrls = new HashSet<string>();
+
+        private ScheduledJobRegistry _registry = new ScheduledJobRegistry();
+        List<Task> runningTasks = new List<Task>();
+
+        public Downloader()
+        {
+            JobManager.Initialize(_registry);
+        }
+        
+        public List<Task> AddTask(DownloadigTask dTask, bool another = false)
+        {
+            Task t1 = new Task(() => AddNewTask(dTask));
+            t1.Start();
+            runningTasks.Add(t1);
+            return runningTasks;
+        }
+
+        private void AddNewTask(DownloadigTask dTask)
+        {
+            bool done = false;
+            string hname = GetMd5StringHash(dTask.url);
+            if (!hashUrls.Contains(hname))
+            {
+                Console.WriteLine("Adding task to scheduler " + hname);
+                hashUrls.Add(hname);
+                JobManager.AddJob(() => DownloadData(dTask), (s) => s.WithName(hname).ToRunNow());
+                JobManager.JobEnd += (info) =>
+                {
+                    if (info.Name.Equals(hname))
+                    {
+                        Console.WriteLine(info.Name + " task done");
+                        done = true;
+                    }    
+                };
+                while (!done)
+                {
+                    Thread.Sleep(300);
+                }
+            }
+            
+        }
+
+        private void DownloadData(DownloadigTask dTask)
+        {
+            WebClient wb = new WebClient();
+            Console.WriteLine("Start fetching data");
+            string result = wb.DownloadString(dTask.url);
+            Console.WriteLine("Data fetched");
+            ValidateData(result, dTask);
+        }
+
+        private void ValidateData(string result, DownloadigTask dTask)
+        {
+            Console.WriteLine("Validating result");
+            /*some awesom validating methods*/
+            dTask.result = result;
+            Console.WriteLine(result);
+        }
+        
+        private string GetMd5StringHash(string input)
+        {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+        
+    }
+    
+    
+    
+    
+    public class ScheduledJobRegistry : Registry
+    {
+        public ScheduledJobRegistry()
+        {
+            Console.WriteLine("Registry initialized");
+            /* Some initial jobs may be here */
+        }
+    }
+
+    public class DownloadigTask
+    {
+        public string url;
+        public string result = String.Empty;
+
+        public DownloadigTask(string url)
+        {
+            this.url = url;
+        }
+    }
+    
+    
+    
+}
---
+++ CoreDownloader/CoreDownloader/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentScheduler;
+
+namespace CoreDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start the scheduler
+            Downloader dwnl = new Downloader();
+
+            /*DownloadigTask downloadigTask1 = new DownloadigTask("http://export.finam.ru/export9.out/" +
+                                                               "?market=45&em=182439&code=EURUSD000TOD&apply=0&df=1&" +
+                                                               "mf=10&yf=2017&from=01.11.2017&dt=16&mt=10&yt=2017&" +
+                                                               "to=16.11.2017&p=9&f=EURUSD000TOD_171101_171116&e=.txt&" +
+                                                               "cn=EURUSD000TOD&dtf=1&tmf=2&MSOR=1&mstimever=0&sep=1&sep2=1&datf=3&at=1");*/
+            DownloadigTask downloadigTask2 = new DownloadigTask("http://export.finam.ru/export9.out/" +
+                                                               "?market=45&em=182406&code=CNYRUB_TOM&apply=0&df=1&" +
+                                                                "mf=10&yf=2017&from=01.11.2017&dt=16&mt=10&yt=2017&" +
+                                                                "to=16.11.2017&p=9&f=CNYRUB_TOM_171101_171116&e=.txt&" +
+                                                                "cn=CNYRUB_TOM&dtf=1&tmf=2&MSOR=1&mstimever=0&sep=1&sep2=1&datf=3&at=1");
+            DownloadigTask downloadigTask3 = new DownloadigTask("http://export.finam.ru/export9.out/" +
+                                                               "?market=45&em=456495&code=BYNRUB_TOM&apply=0&df=1&" +
+                                                                "mf=10&yf=2017&from=01.11.2017&dt=16&mt=10&yt=2017&" +
+                                                                "to=16.11.2017&p=9&f=BYNRUB_TOM_171101_171116&e=.txt&" +
+                                                                "cn=BYNRUB_TOM&dtf=1&tmf=2&MSOR=1&mstimever=0&sep=1&sep2=1&datf=3&at=1");
+
+            List<Task> rt;
+            //rt = dwnl.AddTask(downloadigTask1);
+            rt = dwnl.AddTask(downloadigTask2);
+            rt = dwnl.AddTask(downloadigTask3);
+            Task.WaitAll(rt.ToArray());
+            
+        }
+
+    }
+    
+    
+}
