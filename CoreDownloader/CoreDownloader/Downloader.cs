06494af7e408c4c1f5fe9ce6cf8f0fc029d73f6a
SergeyGorbatyuk171 <sergeygorbatyuk171@gmail.com>
2017-11-24 13:15:07 +0300

    Added lockers, behavior normalized. But maybe slight more subtle implementation needed

--- CoreDownloader/CoreDownloader/Downloader.cs
+++ CoreDownloader/CoreDownloader/Downloader.cs
@@ -15,7 +15,10 @@ namespace CoreDownloader
         //private IQuoteStorage _storage { get; set; }
         private MD5 md5Hash = MD5.Create();
         HashSet<string> hashUrls = new HashSet<string>();
-
+        private object _lockDownload = new object();
+        private object _lockLists = new object();
+        private object _lockScedule = new object();
+        
         private ScheduledJobRegistry _registry = new ScheduledJobRegistry();
         List<Task> runningTasks = new List<Task>();
 
@@ -28,7 +31,10 @@ namespace CoreDownloader
         {
             Task t1 = new Task(() => AddNewTask(dTask));
             t1.Start();
-            runningTasks.Add(t1);
+            lock (_lockLists)
+            {
+                runningTasks.Add(t1);
+            }
             return runningTasks;
         }
 
@@ -39,8 +45,11 @@ namespace CoreDownloader
             if (!hashUrls.Contains(hname))
             {
                 Console.WriteLine("Adding task to scheduler " + hname);
-                hashUrls.Add(hname);
-                JobManager.AddJob(() => DownloadData(dTask), (s) => s.WithName(hname).ToRunNow());
+                lock (_lockScedule)
+                {
+                    hashUrls.Add(hname);
+                    JobManager.AddJob(() => DownloadData(dTask), (s) => s.WithName(hname).ToRunNow());
+                }
                 JobManager.JobEnd += (info) =>
                 {
                     if (info.Name.Equals(hname))
@@ -59,9 +68,14 @@ namespace CoreDownloader
 
         private void DownloadData(DownloadigTask dTask)
         {
-            WebClient wb = new WebClient();
-            Console.WriteLine("Start fetching data");
-            string result = wb.DownloadString(dTask.url);
+            WebClient wb;
+            string result;
+            wb = new WebClient();
+            lock (_lockDownload)
+            {
+                Console.WriteLine("Start fetching data");
+                result = wb.DownloadString(dTask.url);
+            }
             Console.WriteLine("Data fetched");
             ValidateData(result, dTask);
         }
--- CoreDownloader/CoreDownloader/Program.cs
+++ CoreDownloader/CoreDownloader/Program.cs
@@ -13,26 +13,19 @@ namespace CoreDownloader
             // Start the scheduler
             Downloader dwnl = new Downloader();
 
-            /*DownloadigTask downloadigTask1 = new DownloadigTask("http://export.finam.ru/export9.out/" +
+            DownloadigTask downloadigTask1 = new DownloadigTask("http://export.finam.ru/export9.out/" +
                                                                "?market=45&em=182439&code=EURUSD000TOD&apply=0&df=1&" +
                                                                "mf=10&yf=2017&from=01.11.2017&dt=16&mt=10&yt=2017&" +
                                                                "to=16.11.2017&p=9&f=EURUSD000TOD_171101_171116&e=.txt&" +
-                                                               "cn=EURUSD000TOD&dtf=1&tmf=2&MSOR=1&mstimever=0&sep=1&sep2=1&datf=3&at=1");*/
-            DownloadigTask downloadigTask2 = new DownloadigTask("http://export.finam.ru/export9.out/" +
-                                                               "?market=45&em=182406&code=CNYRUB_TOM&apply=0&df=1&" +
+                                                               "cn=EURUSD000TOD&dtf=1&tmf=2&MSOR=1&mstimever=0&sep=1&sep2=1&datf=3&at=1");
+            DownloadigTask downloadigTask2 = new DownloadigTask("http://export.finam.ru/export9.out/?market=45&em=182406&code=CNYRUB_TOM&apply=0&df=1&" +
                                                                 "mf=10&yf=2017&from=01.11.2017&dt=16&mt=10&yt=2017&" +
                                                                 "to=16.11.2017&p=9&f=CNYRUB_TOM_171101_171116&e=.txt&" +
                                                                 "cn=CNYRUB_TOM&dtf=1&tmf=2&MSOR=1&mstimever=0&sep=1&sep2=1&datf=3&at=1");
-            DownloadigTask downloadigTask3 = new DownloadigTask("http://export.finam.ru/export9.out/" +
-                                                               "?market=45&em=456495&code=BYNRUB_TOM&apply=0&df=1&" +
-                                                                "mf=10&yf=2017&from=01.11.2017&dt=16&mt=10&yt=2017&" +
-                                                                "to=16.11.2017&p=9&f=BYNRUB_TOM_171101_171116&e=.txt&" +
-                                                                "cn=BYNRUB_TOM&dtf=1&tmf=2&MSOR=1&mstimever=0&sep=1&sep2=1&datf=3&at=1");
 
             List<Task> rt;
-            //rt = dwnl.AddTask(downloadigTask1);
+            rt = dwnl.AddTask(downloadigTask1);
             rt = dwnl.AddTask(downloadigTask2);
-            rt = dwnl.AddTask(downloadigTask3);
             Task.WaitAll(rt.ToArray());
             
         }
