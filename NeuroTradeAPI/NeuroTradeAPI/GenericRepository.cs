298936f597a70a4efd7bddb557c956ac545190d3
Natalya Ponomareva <natpon12@gmail.com>
2017-12-04 15:16:07 +0300

    Added pattern repository

---
+++ NeuroTradeAPI/NeuroTradeAPI/GenericRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace NeuroTradeAPI
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private ApplicationContext context;
+        private DbSet<TEntity> dbSet;
+
+        public GenericRepository(ApplicationContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<TEntity> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+        }
+
+        public virtual TEntity GetByID(Guid id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public virtual void Delete(Guid id)
+        {
+            TEntity entityToDelete = dbSet.Find(id);
+            Delete(entityToDelete);
+        }
+
+        public virtual void Delete(TEntity entityToDelete)
+        {
+            if (context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                dbSet.Attach(entityToDelete);
+            }
+            dbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+    }
+}
---
+++ NeuroTradeAPI/NeuroTradeAPI/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace NeuroTradeAPI
+{
+    public interface IRepository<TEntity>
+        where TEntity : class
+    {
+        IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy,
+            string includeProperties = "");
+        TEntity GetByID(Guid id);
+        void Insert(TEntity entity);
+        void Delete(Guid id);
+        void Delete(TEntity entityToDelete);
+        void Update(TEntity entityToUpdate);
+    }
+}
