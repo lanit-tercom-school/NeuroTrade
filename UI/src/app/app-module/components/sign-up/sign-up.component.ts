df67df950f8bb1dcb3f4d864fe02079ab8669dda
ucLh <luchwork@mail.ru>
2018-01-30 12:05:51 +0300

    Initial implementation of a sign-up form validation

--- UI/src/app/app-module/components/sign-up/sign-up.component.ts
+++ UI/src/app/app-module/components/sign-up/sign-up.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { SignUpUser } from '../../../models/user/sign-up-user';
 import {AuthService} from '../../../services/auth.service';
+import {AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -10,10 +11,58 @@ import {AuthService} from '../../../services/auth.service';
 })
 export class SignUpComponent implements OnInit {
 
+  form: FormGroup;
   constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    //this.message = new Message('danger', '');
+    this.form = new FormGroup({
+      'person': new FormGroup({
+        'name': new FormControl(null,[Validators.required,this.personValidator]),
+        'surname': new FormControl(null,[Validators.required,this.personValidator])
+      }),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'passGr': new FormGroup({
+        'password': new FormControl(null, [Validators.required, Validators.minLength(7),this.passwordValidator]),
+        'passConfirm': new FormControl(null)
+      }, this.areEqual)
 
+    });
+  }
+
+  private personValidator (control: FormControl) {
+    const value = control.value;
+    const hasLatinLetters = /[A-Z]|[a-z]/.test(value);
+    const hasRussianLetters = /[а-я]|[А-Я]|[ёЁ]/.test(value);
+    const passwordValid =
+      (hasRussianLetters && !hasLatinLetters) || (!hasRussianLetters && hasLatinLetters);
+    if (!passwordValid) {
+      return {
+        'invalidPersonalData' : true
+      };
+    }
+    return null;
+  }
+
+  private passwordValidator(control: FormControl) {
+    const value = control.value;
+    const hasNumber = /[0-9]/.test(value);
+
+    if (!hasNumber) {
+      return {
+        'hasNumber' : true
+      };
+    }
+    return null;
+  }
+
+  private areEqual(c: AbstractControl): ValidationErrors | null {
+    const keys: string[] = Object.keys(c.value);
+    for (const i in keys) {
+      if (i !== '0' && c.value[ keys[ +i - 1 ] ] !== c.value[ keys[ i ] ]) {
+        return { 'areNotEqual': true };
+      }
+    }
   }
 
   isRegDataValid(user: SignUpUser, passwordConfirmation: string): boolean {
@@ -22,12 +71,12 @@ export class SignUpComponent implements OnInit {
       console.log('Passwords don\'t match');
       return false;
     }
-
     return true;
 
   }
 
   Register(name: string, surname: string, email: string, password: string, passwordConfirmation: string) {
+    console.log(this.form);
 
     const user: SignUpUser = {
       name: name + ' ' + surname,
